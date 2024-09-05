2781085a22a883430e837e2bb973b81403c1251d
Audric Serador <audricciel@gmail.com>
2024-09-04 20:14:31 -0500

    init setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/shadcn-ui/**/*.js',
+    './node_modules/aceternity-ui/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
